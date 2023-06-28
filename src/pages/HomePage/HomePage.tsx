327ee34281242709390999f61cb88a7ad8e587fd
Jlucky94 <lucky10291994@gmail.com>
2023-06-28 17:16:18 +0300

    Store and api created

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useSearchUsersQuery} from "store/github/github.api";
 
 const HomePage = () => {
+    const {isLoading,error,data} = useSearchUsersQuery('JLucky')
     return (
-            <h1 className="text-3xl font-bold underline">Hello react</h1>
-    );
-};
+        <h1 className="text-3xl font-bold underline">Hello react</h1>
+    )
+}
 
 export default HomePage;
