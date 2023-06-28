9817df75d544e357b958d16b0ae11eca8fbfdb53
Jlucky94 <lucky10291994@gmail.com>
2023-06-28 17:32:26 +0300

    Search created

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import {useSearchUsersQuery} from "store/github/github.api";
 
 const HomePage = () => {
-    const {isLoading,error,data} = useSearchUsersQuery('JLucky')
+    const {isLoading, isError, data} = useSearchUsersQuery('JLucky')
     return (
-        <h1 className="text-3xl font-bold underline">Hello react</h1>
+        <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+            {isError && <p className="text-center text-red-600">Something went wrong...</p>}
+
+            <div className="relative w-[560px]">
+                <input
+                    type="text"
+                    className="border py-2 px-4 w-full h-[43px] mb-2"
+                    placeholder="Search for Github username..."
+                />
+                <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, explicabo.
+                </div>
+            </div>
+        </div>
     )
 }
 
